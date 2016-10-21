c0b979861a4251452612bbc9f8f0780367b96abb
Brian Egizi <me@brianegizi.com>
2016-10-21 17:16:19 -0400

    Show that tunnel server can be composed into the final GRPC server

--- gohookd/transport_grpc.go
+++ gohookd/transport_grpc.go
@@ -12,10 +12,41 @@ import (
 	grpctransport "github.com/go-kit/kit/transport/grpc"
 )
 
+type GohookdTunnelServer struct {
+	// Has access to the queue to listen for messages to send to connected clients
+	queue HookQueue
+
+	// Keeps a list of all the sessions and the session ids for sending to correct client
+	sessions map[string]pb.Gohook_TunnelServer
+}
+
+// Tunnel transport handler
+func (s *GohookdTunnelServer) Tunnel(req *pb.TunnelRequest, stream pb.Gohook_TunnelServer) error {
+	// pass the stream and request data down to the service?
+	// that way we can handle the logic there... but... then we are mixing transport and
+	// business logic...
+	// is there some way we can expose an interface to the service?
+	for {
+		err := stream.Send(&pb.TunnelResponse{
+			Event: &pb.TunnelResponse_Hook{
+				Hook: &pb.HookCall{
+					Id: "Hello, World",
+				},
+			},
+		})
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
 type GohookdServer struct {
 	list   grpctransport.Handler
 	create grpctransport.Handler
 	delete grpctransport.Handler
+	GohookdTunnelServer
 }
 
 func MakeGRPCServer(ctx context.Context, endpoints Endpoints, logger log.Logger) *GohookdServer {
@@ -23,6 +54,7 @@ func MakeGRPCServer(ctx context.Context, endpoints Endpoints, logger log.Logger)
 		grpctransport.ServerErrorLogger(logger),
 	}
 	return &GohookdServer{
+		GohookdTunnelServer: GohookdTunnelServer{},
 		list: grpctransport.NewServer(
 			ctx,
 			endpoints.ListEndpoint,
@@ -44,28 +76,6 @@ func MakeGRPCServer(ctx context.Context, endpoints Endpoints, logger log.Logger)
 			EncodeGRPCDeleteResponse,
 			options...,
 		),
-	}
-}
-
-// Tunnel transport handler
-func (s *GohookdServer) Tunnel(req *pb.TunnelRequest, stream pb.Gohook_TunnelServer) error {
-	// pass the stream and request data down to the service?
-	// that way we can handle the logic there... but... then we are mixing transport and
-	// business logic...
-	// is there some way we can expose an interface to the service?
-	for {
-		err := stream.Send(&pb.TunnelResponse{
-			Event: &pb.TunnelResponse_Hook{
-				Hook: &pb.HookCall{
-					Id: "Hello, World",
-				},
-			},
-		})
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		time.Sleep(1 * time.Second)
 	}
 }
 
